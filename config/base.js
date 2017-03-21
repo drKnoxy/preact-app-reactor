0651f5ce35c9c8e870e02c73b5b35677732c9332
Adam Knox <adam.w.knox@gmail.com>
2017-03-21 04:06:49 -0400

    additional webpack config work

--- config/base.js
+++ config/base.js
@@ -1,8 +1,32 @@
+var webpack = require("webpack");
+var path = require("path");
+
 module.exports = {
-    resolve: {
-        alias: {
-            components: 'src/components',
-            screens: 'src/screens',
-        }
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].[chunkhash:8].js",
+    path: path.join(__dirname, "build/static/js")
+  },
+  plugins: [
+    // this assumes your vendor imports exist in the node_modules directory
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "vendor",
+      minChunks: function(module) {
+        return module.context && module.context.indexOf("node_modules") !== -1;
+      }
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "manifest"
+    })
+  ],
+  module: {
+    rules: []
+  },
+  resolve: {
+    alias: {
+      components: "src/components",
+      screens: "src/screens",
+      react: "preact"
     }
-}
+  }
+};
--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,31 @@
-module.exports = {
+var webpack = require("webpack");
+var webpackMerge = require("webpack-merge");
+var baseConfig = require("./base");
 
-}
+module.exports = function(env) {
+  return webpackMerge(baseConfig(), {
+    plugins: [
+      // Tell things to run in production mode
+      new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: false
+      }),
+      new webpack.DefinePlugin({
+        "process.env": {
+          NODE_ENV: JSON.stringify("production")
+        }
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        beautify: false,
+        mangle: {
+          screw_ie8: true,
+          keep_fnames: true
+        },
+        compress: {
+          screw_ie8: true
+        },
+        comments: false
+      })
+    ]
+  });
+};
